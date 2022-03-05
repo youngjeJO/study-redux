23bd364459341e6c469f22dc06fc6171fe062d10
youngjeJO <yj01591@gmail.com>
2022-03-05 22:11:32 +0900

    make count

--- src/index.js
+++ src/index.js
@@ -1,22 +1,44 @@
+import { createStore } from 'redux';
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-let count = 0;
-number.innerHTML = count;
+const reducer = (count = 0, action) => {
+  switch (action.type) {
+    case 'Add':
+      return (count += 1);
 
-const updateCount = () => {
-  number.innerHTML = count;
+    case 'Minus':
+      return (count -= 1);
+    default:
+      return count;
+  }
+
+  if (action.type === 'Add') {
+    return (count += 1);
+  } else if (action.type === 'Minus') {
+    return (count -= 1);
+  }
 };
+const store = createStore(reducer);
+
+store.dispatch({ type: 'Add' });
+store.dispatch({ type: 'Add' });
+
+number.innerText = store.getState();
+store.subscribe(() => {
+  number.innerText = store.getState();
+});
+
 const handleAdd = () => {
-  count += 1;
-  console.log(count);
-  updateCount();
+  store.dispatch({ type: 'Add' });
+  // number.innerText = store.getState();
 };
 
 const handleMinus = () => {
-  count -= 1;
-  updateCount();
+  store.dispatch({ type: 'Minus' });
+  // number.innerText = store.getState();
 };
 
 add.addEventListener('click', handleAdd);
