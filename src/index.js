5d0c125836afd78118d606dbb81b522ec4e7c7db
youngjeJO <yj01591@gmail.com>
2022-03-05 19:43:41 +0900

    vanilla set

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const add = document.getElementById('add');
+const minus = document.getElementById('minus');
+const number = document.querySelector('span');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let count = 0;
+number.innerHTML = count;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const updateCount = () => {
+  number.innerHTML = count;
+};
+const handleAdd = () => {
+  count += 1;
+  console.log(count);
+  updateCount();
+};
+
+const handleMinus = () => {
+  count -= 1;
+  updateCount();
+};
+
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
